69b821a024615638b1bc8d59447a5ad0f0b0b5e1
gaddio <sporter@gaddio.com>
2025-11-05 21:42:26 -0700

    Adjust spacing and color

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "8462076aee0e3128faa4e7e829bed018",
+const RESOURCES = {"flutter_bootstrap.js": "64817a923cdeaaa6812d6bff15ea4eaa",
 "version.json": "2f50842094ce1a45371edc5e0ef1991d",
 "index.html": "a59886247575a7f069dc30a32d503b15",
 "/": "a59886247575a7f069dc30a32d503b15",
-"main.dart.js": "41548a6d9361b085ff10a24144a56e4c",
+"main.dart.js": "87fff4c3f9750c1a16dc34018d1f187f",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
